8d5e0dfbf4d09c84a8a14d31c7e565e5ce103695
Karazuna <kchy12345@gmail.com>
2018-04-17 11:47:45 +0900

    2018-04-17, NodeJS - Module create : webserver test

--- index.js
+++ index.js
@@ -23,11 +23,18 @@ console.log('Circumference of a square = ' + mySquare.circum()); */
 console.log(os.type());
 console.log(os.cpus()); */
 
-const http = require('http');
-let server = http.createServer(function (req, res){
-    res.writeHead(200, {'Content-type': 'text/html'});
-    res.write('Hello, world!');
-    res.end();
-})
-server.listen(8000, 'localhost');
-console.log('Server is running at localhost:8080');
+
+// const http = require('http');
+
+// function onRequest(req, res){
+//     res.writeHead(200, {'Content-type': 'text/html'});
+//     res.write('Hello, world!');
+//     res.end();
+// };
+
+// let server = http.createServer(onRequest);
+// server.listen(8000, 'localhost');
+// console.log('Server is running at localhost:8000');
+
+const myServer = require('./myServer');
+myServer.start();
