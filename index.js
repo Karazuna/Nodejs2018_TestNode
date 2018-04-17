c26357af55a74666458af13f5a265136fcb7f7ed
Karazuna <kchy12345@gmail.com>
2018-04-17 10:19:40 +0900

    Nodejs Practice

---
+++ index.js
@@ -0,0 +1,6 @@
+// global.console.log("흑흑 오늘 밥은 맛있었다");
+// console.log(console);
+// console.log(typeof console === 'object');
+// console.log('Node.js version : ' + process.version);
+// console.log(process.memoryUsage());
+console.log(process.env);
