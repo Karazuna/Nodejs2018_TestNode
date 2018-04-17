de95e37663c9f278a8923af5b2834166c3a300a8
Karazuna <kchy12345@gmail.com>
2018-04-17 10:45:09 +0900

    2018-04-17, NodeJS - Module create

---
+++ commitPush.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "2018-04-17, NodeJS - Module create"
+git push -u origin master
--- index.js
+++ index.js
@@ -3,4 +3,12 @@
 // console.log(typeof console === 'object');
 // console.log('Node.js version : ' + process.version);
 // console.log(process.memoryUsage());
-console.log(process.env);
+// console.log(process.env);
+// console.log(module);
+
+// console.log('Hello, world!!');
+const circle = require('./circle');
+
+console.log('Area of a circle = ' + circle.area(3));
+console.log('Circumference of a circle = ' + circle.circum(3));
+console.log('PI = ' + circle.pi);
