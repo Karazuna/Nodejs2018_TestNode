d00959f7e6608a8a2baf1f81fb66825ea4e50e32
Karazuna <kchy12345@gmail.com>
2018-05-01 12:54:24 +0900

    2018-05-01, NodeJS - string_deep learning + web

--- commitPush.bat
+++ commitPush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "2018-05-01, NodeJS - web datachunk"
+git commit -m "2018-05-01, NodeJS - string_deep learning + web"
 git push -u origin master
--- myHandler.js
+++ myHandler.js
@@ -1,11 +1,14 @@
 const queryString = require('querystring');
 
+const myBrain = require('./myBrain');
+const myData = require('./twitterDonKim');
+
+let trainedNet = myBrain.train(myData.trainingData);
 
 function sleep(millisec) {
     let timeStart = new Date().getTime();
     while (new Date().getTime() < timeStart + millisec);
 }
-
 
 // function start(res) {
 //     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -33,10 +36,11 @@ function start(res) {
     let sBody = '<html>' + '<head>' +
         '<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />' +
         '</head>' + '<body>' +
-        '이름과 별명을 입력하세요.<br>' +
+        // '이름과 별명을 입력하세요.<br>' +
+        '내용을 입력하세요.<br>' +
         '<form action="/hello" method="post">' +
         '<input type="text" name="myName" /><br>' +
-        '<input type="text" name="myNick" /><br>' +
+        // '<input type="text" name="myNick" /><br>' +
         '<button type="submit">입력 완료</button>' +
         '</form>' + '</body>' + '</html>';
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -44,18 +48,28 @@ function start(res) {
     res.end();
 }
 
+function execute(input) {
+    let result = myBrain.getResult(trainedNet, input);
+    let output = result.trump > result.kardashian ? 'Trump' : 'Kardashian';
+    console.log('trump = ' + Math.round(result.trump * 100) + '%, kardashian = ' + Math.round(result.kardashian * 100) + '%');
+    let buffer = ('trump = ' + Math.round(result.trump * 100) + '%, kardashian = ' + Math.round(result.kardashian * 100) + '%');
+    return buffer;
+}
+
 function hello(res, postData) {
     console.log('Request handler \'hello\'');
     let sBody = '<html>' + '<head>' +
         '<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />' +
         '</head>' + '<body>' +
-        '안녕하세요, ' + queryString.parse(postData).myName +
-        '(별명: ' + queryString.parse(postData).myNick + ')님!' +
+        '내용 : ' + queryString.parse(postData).myName +
+        // '(별명: ' + queryString.parse(postData).myNick + ')님!' +
+        '<br>사람 : ' + execute(postData) + //postData 날려서 결과값 통으로 returen 받도록 설계
         '</body>' + '</html>';
+
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(sBody);
-    res.end();
 
+    res.end();
 }
 
 exports.start = start;
