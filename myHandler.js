95159e8046acbe257d4f831cb8c49aff61aa23a4
Karazuna <kchy12345@gmail.com>
2018-05-01 11:36:44 +0900

    2018-05-01, NodeJS - serve blocking & callback test

--- myHandler.js
+++ myHandler.js
@@ -1,28 +1,45 @@
-function sleep(millisec){
+function sleep(millisec) {
     let timeStart = new Date().getTime();
     while (new Date().getTime() < timeStart + millisec);
 }
 
 
-function start(res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
+function start(res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('Hello, start!');
     res.end();
     // return 'Hello, start!!';
 }
 
-function hello(res){
-    // sleep(20000); // millisec, 20sec - blocking 
-    // res.writeHead(200, {'Content-Type': 'text/html'});
-    // res.write('Hello, hello!');
-    // res.end();
-    // // return 'Hello, hello!!';
-    
-    setTimeout(function(){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('Hello, hello!');
-        res.end();        
-    }, 20000); //call back
+// function hello(res) {
+//     // sleep(20000); // millisec, 20sec - blocking 
+//     // res.writeHead(200, {'Content-Type': 'text/html'});
+//     // res.write('Hello, hello!');
+//     // res.end();
+//     // // return 'Hello, hello!!';
+
+//     setTimeout(function () {
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write('Hello, hello!');
+//         res.end();
+//     }, 20000); //call back
+// }
+
+function hello(res, postData) {
+    console.log('Request handler \'start\'');
+    let sBody = '<html>' + '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />' +
+        '</head>' + '<body>' +
+        '이름과 별명을 입력하세요.<br>' +
+        '<form action="/hello" method="post">' +
+        '<input type="text" name="myName" /><br>' +
+        '<input type="text" name="myNick" /><br>' +
+        '<button type="submit">입력 완료</button>' +
+        '</form>' + '</body>' + '</html>';
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(sBody);
+    res.end();
+
 }
 
 exports.start = start;
--- myServer.js
+++ myServer.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const url = require('url');
 
-exports.start = function(port, host, route, handle) {
-// module.exports = function(port, host) {
-    function onRequest(req, res){ //요청 올때 응답에 대한 정의
+exports.start = function (port, host, route, handle) {
+    // module.exports = function(port, host) {
+    function onRequest(req, res) { //요청 올때 응답에 대한 정의
         //let sPathname = url.parse(req.url).pathname;//url 요청에 대한 parse가 pathname으로
         // let content = route(sPathname, handle);
         // route(sPathname);
@@ -11,7 +11,14 @@ exports.start = function(port, host, route, handle) {
         // res.write(content);
         // res.end();
         let sPathname = url.parse(req.url).pathname;
-        route(sPathname, handle, res);
+
+        req.setEncoding('utf-8');
+        req.addListener('data', function (dataChunk) {
+            sPostData += dataChunk;
+            console.log('Chunk = ' + dataChunk);
+        });
+        req.addListener('end',
+            route(sPathname, handle, res, sPostData));
     }
 
     http.createServer(onRequest).listen(port, host);
